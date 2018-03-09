f7a8425ac3994402a8c7c7d4cd6d64f99f99703b
quanqimeng@163.com <qqm654321>
2018-03-10 03:03:37 +0800

    add more swoole http-related config params

--- config/http.php
+++ config/http.php
@@ -25,7 +25,25 @@ return [
 
             'daemonize' => env('HTTP_SERVER_OPTIONS_DAEMONIZE', 1),
             
-            'worker_num' => env('HTTP_SERVER_OPTIONS_WORKERNUM', 4)
+            'worker_num' => env('HTTP_SERVER_OPTIONS_WORKERNUM', 4),
+            
+            'reactor_num' => env('HTTP_SERVER_OPTIONS_REACTOR_NUM', 4),
+            
+            'max_request' => env('HTTP_SERVER_OPTIONS_MAX_REQUEST', 4),
+            
+            'dispatch_mode' => env('HTTP_SERVER_OPTIONS_DISPATCH_MODE', 3),
+            
+            'log_level' => env('HTTP_SERVER_OPTIONS_Log_level', 5),
+            
+            'open_http2_protocol' => env('HTTP_SERVER_OPTIONS_HTTP2', false),
+            'package_max_length' => env('HTTP_SERVER_OPTIONS_PACKAGE_MAXLENGTH', 20480),//bytes need use with http2
+            
+            //系统需要开启ptrace  kernel.yama.ptrace_scope=0
+            'request_slowlog_timeout' => env('HTTP_SERVER_OPTIONS_SLOW_TIMEOUT', 2),//second
+            
+            'request_slowlog_file' => env('HTTP_SERVER_OPTIONS_SLOW_FILE', base_path('storage/logs/httpslow.log')),//
+            
+            'trace_event_worker' => true,//跟踪 Task 和 Worker 进程
 
         ],
 
